089517b21d8e923b6558cb89fd4a1db7da1d47ca
unknown <hassanetinde@gmail.com>
2024-09-18 04:06:13 +0000

    third commit

--- src/Components/AllMenus.jsx
+++ src/Components/AllMenus.jsx
@@ -3,7 +3,7 @@ export const AllMennus = ()=>{
           <div className="relative w-full mt-24">
                <h2 className="absolute z-10 text-white text-4xl md:text-7xl font-bold flex justify-center items-center w-full h-full">NOUS VOUS ATTENDONS !</h2>
                <div className="absolute bg-black w-full h-full opacity-75"></div>
-               <img className="w-full h-full" src="../../public/menus.jpeg" alt="" />
+               <img className="w-full h-full" src="/menus.jpeg" alt="" />
           </div>
      )
 }
